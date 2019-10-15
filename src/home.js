d40e3260823619b38747e1975251923b4284d094
mohamadD <rediab88@gmail.com>
2019-10-15 12:01:07 +0300

    add embed html

--- src/home.js
+++ src/home.js
@@ -1,25 +1,47 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
+const home = (request, response) => {
+  const filePath = path.join(__dirname, "..", "public", "index.html");
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      console.log(error);
+      response.writeHead(500, { "Content-Type": "text/html" });
+      response.end("<h1>Sorry, we've had a problem on our end</h1>");
+    } else {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.end(file);
+    }
+  });
+};
 
+const handlePublic = (request, response, url) => {
+  console.log(request.url);
 
+  const extension = request.url.split(".")[1];
+  const extensionType = {
+    html: "text/html",
+    css: "text/css",
+    js: "application/javascript",
+    ico: "image/x-icon"
+  };
 
+  const filePath = path.join(__dirname, "..", request.url);
 
-module.exports = ((request, response) => {
-  
-    const filePath = path.join(__dirname, '..', 'public', 'index.html');
-    fs.readFile(filePath, (error, file) => {
-      if (error) {
-        console.log(error);
-        response.writeHead(500, { 'Content-Type': 'text/html' });
-        response.end("<h1>Sorry, we've had a problem on our end</h1>");
-      } else {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.end(file);
-      }
-    });
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      console.log(error);
+      response.writeHead(404, { "Content-Type": "text/html" });
+      response.end("<h1>404 file not found</h1>");
+    } else {
+      response.writeHead(200, { "Content-Type": extensionType[extension] });
+      response.end(file);
+    }
+  });
+  console.log(url);
+};
 
-
-
-  
-  })
+module.exports = {
+  home,
+  handlePublic
+};
--- src/router.js
+++ src/router.js
@@ -1,27 +1,25 @@
-const url = require('url');
-const home = require('./home');
-const movie = require('./movie');
-const notFound = require('./notfound.js');
+const url = require("url");
+const home = require("./home.js");
+const movie = require("./movie.js");
+const notFound = require("./notfound.js");
 
-
-const router = ((request, response) => {
-    // basic route logger
+const router = (request, response) => {
+  // basic route logger
   console.log(`${request.method} ${request.url}`);
-  
-  // grab pathname 
+
+  // grab pathname
   const { pathname } = url.parse(request.url);
-  
+
   // router
-  switch(pathname){
-    case `/`:
-      return home(request, response);
-
-    case `/movie`:
-      return movie(request,response);
-
-    default:
-      return notFound(request, response);
-    }
-})
+  if (pathname == "/") {
+    return home.home(request, response);
+  } else if (pathname == "/movie") {
+    return movie(request, response);
+  } else if (pathname.indexOf("public") !== -1) {
+    home.handlePublic(request, response, url);
+  } else {
+    return notFound(request, response);
+  }
+};
 
 module.exports = router;
