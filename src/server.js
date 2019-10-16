bfdc36fc545ec535b143066a5946507f8077c748
nataphilips <fnd.natalia@gmail.com>
2019-10-16 18:47:57 +0300

    removing hostname from listen

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const http = require("http");
-const hostname = process.env.HOSTNAME || "localhost";
+const hostname = process.env.HOST_NAME || "localhost";
 const port = process.env.PORT || 3000;
 const router = require("./router");
 
-http.createServer(router).listen(port, hostname, () => {
+http.createServer(router).listen(port, () => {
   console.log(`server running at port http://${hostname}:${port}`);
 });
