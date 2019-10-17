7bc15480bbd29e2d8ccc421a2d4afbb69a71259d
ebraheemabbas <ebraheemabbas51@gmail.com>
2019-10-17 10:15:30 +0300

    reslove conflict

---
+++ src/servertest.js
@@ -0,0 +1,41 @@
+const test = require('tape');
+const supertest = require('supertest');
+const router = require('./router');
+test('Initialise', (t) => {
+    let num = 2
+    t.equal(num, 2, 'Should return 2');
+    t.end(); // Remember to call t.end() after every test call, to ensure tests run in order. You can also investigate t.plan() in the docs
+  })
+  test('Home route returns a status code of 200', (t) => {
+    supertest(router)
+        .get("/")
+        .expect(200)
+        .expect('Content-Type', /html/)
+        .end((err, res) => {
+            t.error(err);
+            t.equal(res.statusCode, 200, 'Should return 200'); // note we have used .expect(200) above so this assertion is not neccesary. This is to show you how to check the statusCode in the res.
+            t.end();      
+          });
+});
+test('movie route returns a status code of 200', (t) => {
+    supertest(router)
+        .get("/movie")
+        .expect(200)
+        .expect('Content-Type', /html/)
+        .end((err, res) => {
+            t.error(err);
+            t.equal(res.statusCode, 200, 'Should return 200'); // note we have used .expect(200) above so this assertion is not neccesary. This is to show you how to check the statusCode in the res.
+            t.end();      
+          });
+});
+test('else route returns a status code of 404', (t) => {
+    supertest(router)
+        .get("/else")
+        .expect(404)
+        .expect('Content-Type', /html/)
+        .end((err, res) => {
+            t.error(err);
+            t.equal(res.statusCode, 404, 'Should return 404'); // note we have used .expect(200) above so this assertion is not neccesary. This is to show you how to check the statusCode in the res.
+            t.end();      
+          });
+});
